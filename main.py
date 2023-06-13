2bab48409b137fbf885fab5177bd3cda9fd990ea
anindyasasi <87769831+anindyasasi@users.noreply.github.com>
2023-06-13 20:10:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -18,14 +18,28 @@ load_dotenv()
 # Initialize Flask application
 app = Flask(__name__)   
 
-cred = credentials.Certificate('key.json')  # Replace with your own service account key file path
+
+cred_json= {
+  "type": "service_account",
+  "project_id": "fundup-387016",
+  "private_key_id": "a67c3cae82bddaa3b37aaf9f926184c06efd267b",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCwpx7o9BdA7rMJ\nqpyqI4lPM/Slvj0FZwKUpF2Y9oNhxNjFECqp/U32E+ZWDYXhMGyqP6/ww4mZfK1T\ndHJTmQf2+pMQdzeLfylT+M9+jZj7QS9iErYI1BupA5BXpajxvWFG2X+L8Lp8Vxn9\ncb+8pzLHsH8+ty4WVC32YeszlqZSfwXeYEFzeiGRQBJVmaaBtstzCLzcjQ5l1nZi\nk3VD9OsEIKU3hXkNPuxiCUQyvuqdXlhKAmfZPQpw4jmU4ECQO2mpCV+lFkLtJstc\nelurpTp6h2Dm6RxLfsGKcEyW8SEdRQNeyLOyV5gCpM+1OrRvbyy1gF3ycB3u9kD8\nW1anniuFAgMBAAECggEAR2FIJqZW7RhmxN2pT0Brv9LFJOHhg1jT3J8r6N6XSP7C\n/qHhM24Uvf3dgWEWe19XUVXJsJY6eAg+ey3e8nOwGba3jRw3GAlqeDFeGot5yPDW\nhiD8aEXY5Wr4vMnGIeQ9teS12qSLnimN6XC4orDG3pStXfijyUb7iYaYhPB3RXa9\nGt3cLTc0+JfH17DqAwchxhRemQDZ4Wi7fjL2Arp6AGIcouMA6TokXMackZVe7h32\n4LHy+VR8+gHC4TqdeLxb1yvnLPf8ELCaf2EbnRCUvWGs079/dZzboE1VnkNUPmDa\nOUpEhGb/16w3kfDPISN4QNA18NYn2c9KcjYOBQe20wKBgQDgcPUx7GnAADkM+A14\nb0esBgZVLIykG+FnIgV5ckdfZVjWali/AG5q8jcA5Dx+/ktwQH86cOsHIyeGdbEh\nB4xSr5q5QLxLNkPQ5tQSqW2sFJtwZbQ3IatGfVsLb5iQYESja/WmVlWMDTjM5zbT\nzciC6MQ33n0VEbDIVXvc2itl+wKBgQDJffWAzmYaQQoIa6ZBTOxzBS7eBOnPSxjO\nAme1oQmzK3hM48ojdodgtaR07RgrO82bjxlpnJYHIITISvxiIdeEVVrFYFo/Zje5\nMS+aGYSK05TdmzGMX+Vmtb2tCu9OCHoD6AztIeTXB41ST3z2XlVB6MPgCgUxSG4a\nxjzpQkN8fwKBgBjuc05IZLbfT3cRVu257sw9Hxb3C+hu8Gr0bIdBGoyORYAL8C/H\nbHyUy2dd8xpoRRkDER78zB7O2OUmzbZNkFjfCODrP/9a182s1oH8MCKdZ2bk5U/6\nfXwnEKYEj336M6WzqGYB0R7tmRGp3X1JrqxcDu/l1x8wB+M5G7k8wvVhAoGBAIP/\nHTZ9gAvQ8bakduyubPPIwHQ3ucfPxXcnwjMNRSJ35r5QN5rVykgDlrH2pG+mJMK0\nkwxJxUrz9aiU3xOWYe5SUD2fKmAAIZ8TZsDH2LltdEdcpK/2Hn0TsCdNU4nGKdCn\nUtiB7L0lOGJkqlNnZujfiHobdl1buq2Vkk+o1jcXAoGAGH8gZCVj/+zghtn9NFK8\nyZXb9lYKXJegVPeKpnwrvu8U8LmuFsKwcIgvA1beHV8IpXEQJ3toeZsU6BC+std1\nADq7NaTKywd6aREV9bwMc6nbKGvf8zeX/g17d3eADBIPcXs/rvJEVlLnjvYyNT9d\nTnL/hKlL9q9Tb87Yxctl0Js=\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-7vr3t@fundup-387016.iam.gserviceaccount.com",
+  "client_id": "104989193780059513595",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-7vr3t%40fundup-387016.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
+
+  
+cred = credentials.Certificate(cred_json)  # Replace with your own service account key file path
 
 
 # Initialize Firestore
 firebase_admin.initialize_app(cred)
 db = firestore.client()
-
-
 
         
 @app.route('/startup', methods=['POST'])
@@ -115,10 +129,105 @@ def add_startup():
 
     addRecStartup(startup_ref, investor_features_ref)
         
-    #     return jsonify({'message': 'Startup data added successfully'})
+    return jsonify({'message': 'Startup data added successfully'})
+     #except Exception as e:
+         #return jsonify({'error': str(e)}), 500
+
+
+@app.route('/investor', methods=['POST'])
+def add_investor():
+
+    data = request.get_json()
+
+    email_investor = data['email_investor']
+    isActive = data['isActive']
+    nama_lengkap = data['nama_lengkap']
+    nik_investor = data['nik_investor']
+    pengalaman_investasi = data['pengalaman_investasi']
+    target_industri = data['target_industri']
+    target_investasi = data['target_investasi']
+    target_perkembangan = data['target_perkembangan']
+    tipe_investor = data['tipe_investor']
+    tipe_startup = data['tipe_startup']
+    
+    investor_data = {
+        'email_investor': email_investor,
+        'isActive': isActive,
+        'nama_lengkap': nama_lengkap,
+        'nik_investor': nik_investor,
+        'pengalaman_investasi': pengalaman_investasi,
+        'target_industri': target_industri,
+        'target_investasi': target_investasi,
+        'target_perkembangan': target_perkembangan,
+        'tipe_investor': tipe_investor,
+        'tipe_startup': tipe_startup
+    }
+    
+    # try:
+    # Add investor data to Firestore
+    investor_ref = db.collection('investor_loker')
+    doc_ref = investor_ref.document()
+    doc_ref.set(investor_data)
+
+    # Initialize startup_features_ref
+    startup_features_ref = db.collection('startup')
+    def addRecInvestor(investor_features_ref, startup_features_ref):
+        investor_features_docs = investor_features_ref.stream()
+          
+        investor_features = []
+        investor_ids = []
+        for doc in investor_features_docs:
+            data = doc.to_dict()
+            investor_features.append(data['target_perkembangan'] + ' ' + data['target_industri'])
+            investor_ids.append(str(doc.id))
+            
+        startup_features_docs = startup_features_ref.stream()            
+        startup_features = []
+        startup_ids = []
+        for doc in startup_features_docs:
+            data = doc.to_dict()
+            startup_features.append(data['tingkat_perkembangan_perusahaan'] + ' ' + data['industri_startup'])
+            startup_ids.append(doc.id)      
+
+        tokenizer = Tokenizer()
+        tokenizer.fit_on_texts(investor_features + startup_features)
+        investor_sequences = tokenizer.texts_to_sequences(investor_features)
+        investor_padded = pad_sequences(investor_sequences)
+            
+        startup_sequences = tokenizer.texts_to_sequences(startup_features)
+        startup_padded = pad_sequences(startup_sequences)
+            
+        # Convert padded sequences to tensors
+        investor_tensors = tf.convert_to_tensor(investor_padded, dtype=tf.float32)
+        startup_tensors = tf.convert_to_tensor(startup_padded, dtype=tf.float32)
+            
+        # Calculate cosine similarity between investor and startup tensors
+        similarity_matrix = cosine_similarity(investor_tensors, startup_tensors)
+            
+        def get_startup_matches(investor_id):
+            matches = {}
+            investor_index = investor_ids.index(investor_id)
+            similarities = similarity_matrix[investor_index]
+            sorted_indexes = np.argsort(similarities)[::-1]
+            top_matches = [startup_ids[i] for i in sorted_indexes[:20]]
+            matches[investor_id] = top_matches
+            return matches
+            
+            # Add investor matches to Firestore collection
+        def add_startup_matches(investor_id, startup_matches):
+            matches_ref = db.collection('startup_matches')
+            matches_ref.document(investor_id).set({ 'startup_matches': startup_matches })
+            
+        for id in investor_ids:
+            input_id = id
+            startup_matches = get_startup_matches(input_id)
+            add_startup_matches(input_id, startup_matches[input_id])
+
+    addRecInvestor(investor_ref, startup_features_ref)
+        
+    return jsonify({'message': 'Investor data added successfully'})
     # except Exception as e:
     #     return jsonify({'error': str(e)}), 500
-
 
 
 @app.route('/get-recommendation', methods=['POST'])
@@ -217,4 +326,4 @@ def get_recomendation_for_startup():
 
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(debug=True)
